4fc89e917ed1fe96639f44840cce1ba82eb8f799
nik00726 <nik00726@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-12 08:53:12 +0000

    Added support for $.browser if not set in jquery
    git-svn-id: https://plugins.svn.wordpress.org/wp-responsive-video-gallery-with-lightbox/trunk@1065720 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/vl-box-js.js
+++ js/vl-box-js.js
@@ -17,6 +17,41 @@
  */
 
 ;(function($) {
+  
+    var matched, browser;
+
+	$.uaMatch = function( ua ) {
+	    ua = ua.toLowerCase();
+
+	    var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+		/(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+		/(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+		/(msie) ([\w.]+)/.exec( ua ) ||
+		ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+		[];
+
+	    return {
+		browser: match[ 1 ] || "",
+		version: match[ 2 ] || "0"
+	    };
+	};
+
+	matched = $.uaMatch( navigator.userAgent );
+	browser = {};
+
+	if ( matched.browser ) {
+	    browser[ matched.browser ] = true;
+	    browser.version = matched.version;
+	}
+
+	// Chrome is Webkit, but Webkit is also Safari.
+	if ( browser.chrome ) {
+	    browser.webkit = true;
+	} else if ( browser.webkit ) {
+	    browser.safari = true;
+	}
+
+	$.browser = browser;
 	var tmp, loading, overlay, wrap, outer, content, close, title, nav_left, nav_right,
 
 		selectedIndex = 0, selectedOpts = {}, selectedArray = [], currentIndex = 0, thisObj=null,currentOpts = {}, currentArray = [],
